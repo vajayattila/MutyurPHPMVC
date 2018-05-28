5ced0e426d9843d8b92707f9dfe319a844ca5b47
Vajay Attila <vajay.attila@gmail.com>
2018-05-28 15:39:23 +0200

    Refactory
    Refactory

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *  @file index.php
+ *  @brief Index file for MutyurPHPMVC. Project home: https://github.com/vajayattila/MutyurPHPMVC
+ *	@author Vajay Attila (vajay.attila@gmail.com)
+ *  @copyright MIT License (MIT)
+ *  @date 2017.04.07-2018.05.28
+ *  @version 1.0.0.0
+ */
+
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', E_ALL);
+
+
+function APPPATH(){
+    return __dir__.'/'; 
+}
+
+function WITHOUT_APPPATH($url){
+    $prefix = APPPATH();
+    if (substr($url, 0, strlen($prefix)) == $prefix) {
+        $url = substr($url, strlen($prefix));
+    } 
+    return $url;
+}
+
+
+function CondDefine($defname, $value)
+{
+    if(!defined($defname))
+    {
+        define($defname, $value);
+    }
+}
+
+require_once('application/core/workframe.php'); 
+
+// Eof index.php
